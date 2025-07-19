e5488e49bea8505d8b8f8252de816da8c11a1108
ShubhPundir <shb.pndr@gmail.com>
2025-07-19 13:32:49 +0530

    refactor: directory restructured

--- PythonAgent/system_info/utils.py
+++ PythonAgent/system_info/utils.py
@@ -3,6 +3,9 @@ import os, sys, re
 def get_company_info_from_filename():
     try:
         filename = os.path.basename(sys.argv[0])
+        # depends on the name of the main.py file
+        # TODO
+        # Should be entered once when installed and registered as an app
         base_name = os.path.splitext(filename)[0]
         return re.sub(r"(?:\s*-\s*Copy(?:\s*\(.*\))?|\s*\(.*\)|_\d+)$", "", base_name, flags=re.IGNORECASE)
     except:
